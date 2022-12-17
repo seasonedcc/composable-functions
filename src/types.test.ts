e719b32fc2cd5516683ded766e963f391a1f8fc4
Gustavo Guichard <gustavoguichard@gmail.com>
2022-12-16 23:39:02 -0300

    Add type tests

---
+++ src/types.test.ts
@@ -0,0 +1,94 @@
+// deno-lint-ignore-file ban-ts-comment no-namespace no-unused-vars require-await
+import { makeDomainFunction } from './domain-functions.ts'
+import { describe, it } from 'https://deno.land/std@0.156.0/testing/bdd.ts'
+import { assertEquals } from 'https://deno.land/std@0.160.0/testing/asserts.ts'
+import * as Subject from './types.ts'
+
+type Expect<T extends true> = T
+type Equal<A, B> =
+  // prettier is removing the parens thus worsening readability
+  // prettier-ignore
+  (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2)
+    ? true
+    : false
+
+namespace UnpackData {
+  const result = makeDomainFunction()(async () => ({ name: 'foo' } as const))
+
+  type test = Expect<
+    Equal<Subject.UnpackData<typeof result>, { readonly name: 'foo' }>
+  >
+  type error = Expect<
+    // @ts-expect-error
+    Equal<Subject.UnpackData<typeof result>, { name: string }>
+  >
+}
+
+namespace UnpackResult {
+  const result = makeDomainFunction()(async () => ({ name: 'foo' }))
+
+  type test = Expect<
+    Equal<Subject.UnpackResult<typeof result>, Subject.Result<{ name: string }>>
+  >
+}
+
+namespace UnpackSuccess {
+  const result = makeDomainFunction()(async () => ({ name: 'foo' }))
+
+  type test = Expect<
+    Equal<
+      Subject.UnpackSuccess<typeof result>,
+      Subject.SuccessResult<{ name: string }>
+    >
+  >
+}
+
+namespace MergeObjs {
+  const obj1 = { a: 1, b: 2 } as const
+  const obj2 = {}
+  const obj3 = { c: 3, d: 4 } as const
+
+  type Result = Subject.MergeObjs<[typeof obj1, typeof obj2, typeof obj3]>
+
+  type test1 = Expect<Equal<keyof Result, 'a' | 'b' | 'c' | 'd'>>
+  type test2 = Expect<Equal<Result[keyof Result], 1 | 2 | 3 | 4>>
+}
+
+namespace TupleToUnion {
+  type Result = Subject.TupleToUnion<[1, 2, 3]>
+
+  type test = Expect<Equal<Result, 1 | 2 | 3>>
+}
+
+namespace Last {
+  type test1 = Expect<Equal<Subject.Last<[1, 2, 3]>, 3>>
+  type test2 = Expect<Equal<Subject.Last<[1]>, 1>>
+  type test3 = Expect<Equal<Subject.Last<[]>, never>>
+}
+
+namespace AtLeastOne {
+  type Result = Subject.AtLeastOne<{ a: 1; b: 2 }>
+
+  const test1: Result = { a: 1 }
+  const test2: Result = { b: 2 }
+  const test3: Result = { a: 1, b: 2 }
+  // @ts-expect-error
+  const error1: Result = {}
+  // @ts-expect-error
+  const error2: Result = { a: 1, c: 3 }
+}
+
+namespace ListToResultData {
+  const dfA = makeDomainFunction()(async () => ({ a: 1 } as const))
+  const dfB = makeDomainFunction()(async () => ({ b: 2 } as const))
+
+  type Result = Subject.List.Map<
+    Subject.ListToResultData,
+    [typeof dfA, typeof dfB]
+  >
+
+  type test = Expect<Equal<Result, [{ readonly a: 1 }, { readonly b: 2 }]>>
+}
+
+describe('type tests', () =>
+  it('should have no ts errors', () => assertEquals(true, true)))
