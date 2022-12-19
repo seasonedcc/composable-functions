bbd1d9f36ad124b85c20e6fb1d56aef4fa1a97d0
Gustavo Guichard <gustavoguichard@gmail.com>
2022-12-19 11:24:11 -0300

    Make all type exports explicit

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,21 @@ export * from './domain-functions.ts'
 export * from './input-resolvers.ts'
 export * from './errors.ts'
 export * from './deprecated-types.ts'
-export * from './types.ts'
 export { mergeObjects } from './utils.ts'
+export type {
+  AtLeastOne,
+  DomainFunction,
+  ErrorData,
+  ErrorResult,
+  ErrorWithMessage,
+  Last,
+  MergeObjs,
+  Result,
+  SchemaError,
+  SuccessResult,
+  TupleToUnion,
+  UnpackAll,
+  UnpackData,
+  UnpackResult,
+  UnpackSuccess,
+} from './types.ts'
