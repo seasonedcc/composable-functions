874eadcc84d400a1b9528538722dc5e2419073b2
Gustavo Guichard <gustavoguichard@gmail.com>
2023-08-10 15:48:20 -0300

    feat: Adds a collectSequence combinator

---
+++ src/collect-sequence.test.ts
@@ -0,0 +1,176 @@
+import { describe, it } from 'https://deno.land/std@0.156.0/testing/bdd.ts'
+import { assertEquals } from 'https://deno.land/std@0.160.0/testing/asserts.ts'
+import { z } from 'https://deno.land/x/zod@v3.21.4/mod.ts'
+
+import { makeDomainFunction } from './constructor.ts'
+import { collectSequence } from './domain-functions.ts'
+import type { DomainFunction } from './types.ts'
+import type { Equal, Expect } from './types.test.ts'
+
+describe('collectSequence', () => {
+  it('should compose domain functions keeping the given order of keys', async () => {
+    const a = makeDomainFunction(z.object({ id: z.number() }))(({ id }) => ({
+      id: id + 2,
+    }))
+    const b = makeDomainFunction(z.object({ id: z.number() }))(
+      ({ id }) => id - 1,
+    )
+
+    const c = collectSequence({ a, b })
+    type _R = Expect<
+      Equal<typeof c, DomainFunction<{ a: { id: number }; b: number }>>
+    >
+
+    assertEquals(await c({ id: 1 }), {
+      success: true,
+      data: { a: { id: 3 }, b: 2 },
+      errors: [],
+      inputErrors: [],
+      environmentErrors: [],
+    })
+  })
+
+  it('should use the same environment in all composed functions', async () => {
+    const a = makeDomainFunction(
+      z.undefined(),
+      z.object({ env: z.number() }),
+    )((_input, { env }) => ({
+      inp: env + 2,
+    }))
+    const b = makeDomainFunction(
+      z.object({ inp: z.number() }),
+      z.object({ env: z.number() }),
+    )(({ inp }, { env }) => inp + env)
+
+    const c = collectSequence({ a, b })
+    type _R = Expect<
+      Equal<typeof c, DomainFunction<{ a: { inp: number }; b: number }>>
+    >
+
+    assertEquals(await c(undefined, { env: 1 }), {
+      success: true,
+      data: { a: { inp: 3 }, b: 4 },
+      errors: [],
+      inputErrors: [],
+      environmentErrors: [],
+    })
+  })
+
+  it('should fail on the first environment parser failure', async () => {
+    const envParser = z.object({ env: z.number() })
+    const a = makeDomainFunction(
+      z.undefined(),
+      envParser,
+    )((_input, { env }) => ({
+      inp: env + 2,
+    }))
+    const b = makeDomainFunction(
+      z.object({ inp: z.number() }),
+      envParser,
+    )(({ inp }, { env }) => inp + env)
+
+    const c = collectSequence({ a, b })
+    type _R = Expect<
+      Equal<typeof c, DomainFunction<{ a: { inp: number }; b: number }>>
+    >
+
+    assertEquals(await c(undefined, {}), {
+      success: false,
+      errors: [],
+      inputErrors: [],
+      environmentErrors: [{ message: 'Required', path: ['env'] }],
+    })
+  })
+
+  it('should fail on the first input parser failure', async () => {
+    const firstInputParser = z.undefined()
+
+    const a = makeDomainFunction(
+      firstInputParser,
+      z.object({ env: z.number() }),
+    )((_input, { env }) => ({
+      inp: env + 2,
+    }))
+    const b = makeDomainFunction(
+      z.object({ inp: z.number() }),
+      z.object({ env: z.number() }),
+    )(({ inp }, { env }) => inp + env)
+
+    const c = collectSequence({ a, b })
+    type _R = Expect<
+      Equal<typeof c, DomainFunction<{ a: { inp: number }; b: number }>>
+    >
+
+    assertEquals(await c({ inp: 'some invalid input' }, { env: 1 }), {
+      success: false,
+      errors: [],
+      inputErrors: [
+        { message: 'Expected undefined, received object', path: [] },
+      ],
+      environmentErrors: [],
+    })
+  })
+
+  it('should fail on the second input parser failure', async () => {
+    const a = makeDomainFunction(
+      z.undefined(),
+      z.object({ env: z.number() }),
+    )(() => ({
+      inp: 'some invalid input',
+    }))
+    const b = makeDomainFunction(
+      z.object({ inp: z.number() }),
+      z.object({ env: z.number() }),
+    )(({ inp }, { env }) => inp + env)
+
+    const c = collectSequence({ a, b })
+    type _R = Expect<
+      Equal<typeof c, DomainFunction<{ a: { inp: string }; b: number }>>
+    >
+
+    assertEquals(await c(undefined, { env: 1 }), {
+      success: false,
+      errors: [],
+      inputErrors: [
+        { message: 'Expected number, received string', path: ['inp'] },
+      ],
+      environmentErrors: [],
+    })
+  })
+
+  it('should compose more than 2 functions', async () => {
+    const a = makeDomainFunction(z.object({ aNumber: z.number() }))(
+      ({ aNumber }) => ({
+        aString: String(aNumber),
+      }),
+    )
+    const b = makeDomainFunction(z.object({ aString: z.string() }))(
+      ({ aString }) => ({
+        aBoolean: aString == '1',
+      }),
+    )
+    const c = makeDomainFunction(z.object({ aBoolean: z.boolean() }))(
+      ({ aBoolean }) => !aBoolean,
+    )
+
+    const d = collectSequence({ a, b, c })
+    type _R = Expect<
+      Equal<
+        typeof d,
+        DomainFunction<{
+          a: { aString: string }
+          b: { aBoolean: boolean }
+          c: boolean
+        }>
+      >
+    >
+
+    assertEquals(await d({ aNumber: 1 }), {
+      success: true,
+      data: { a: { aString: '1' }, b: { aBoolean: true }, c: false },
+      errors: [],
+      inputErrors: [],
+      environmentErrors: [],
+    })
+  })
+})
