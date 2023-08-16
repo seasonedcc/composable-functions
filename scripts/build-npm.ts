5df95031c76bd629d2a8955067b6dc99e7fe996e
Diogo Biazus <diogo@biazus.ca>
2023-08-16 17:04:19 -0400

    Upgrade dnt version and update deno.lock after building npm

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,5 +1,5 @@
 // ex. scripts/build_npm.ts
-import { build, emptyDir } from 'https://deno.land/x/dnt@0.33.1/mod.ts'
+import { build, emptyDir } from 'https://deno.land/x/dnt@0.38.0/mod.ts'
 import pkg from '../deno.json' assert { type: 'json' }
 
 await emptyDir('./npm')
