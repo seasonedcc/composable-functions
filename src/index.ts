1f38de458dff102c01dd57c3e087af8e974ec5c7
Diogo Biazus <diogo@biazus.ca>
2023-11-06 20:28:26 -0500

    mergeObjects for now resides in atmp

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { makeDomainFunction, mdf, safeResult } from './constructor.ts'
 export * from './domain-functions.ts'
 export * from './input-resolvers.ts'
 export * from './errors.ts'
-export { mergeObjects } from './utils.ts'
+export { mergeObjects } from './atmp/atmp.ts'
 export type {
   AtLeastOne,
   DomainFunction,
