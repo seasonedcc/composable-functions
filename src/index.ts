c219717ade2e3d47598e854b7db79cf53714ba39
Diogo Biazus <diogo@biazus.ca>
2023-11-16 11:36:34 -0500

    Add command to generate docs and add composable namespace to public interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-export { makeDomainFunction, mdf } from './constructor.ts'
+export {
+  fromComposable,
+  makeDomainFunction,
+  mdf,
+  toComposable,
+} from './constructor.ts'
 export * from './domain-functions.ts'
 export * from './input-resolvers.ts'
 export * from './errors.ts'
 export { mergeObjects } from './composable/composable.ts'
+export type { Composable } from './composable/index.ts'
+import * as composable from './composable/index.ts'
+export { composable }
 export type {
   AtLeastOne,
   DomainFunction,
@@ -23,3 +31,4 @@ export type {
   UnpackResult,
   UnpackSuccess,
 } from './types.ts'
+
