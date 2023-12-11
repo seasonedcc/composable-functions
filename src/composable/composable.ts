f8d973a8171c3a52b455bf6fea02b099aec64bed
Diogo Biazus <diogo@biazus.ca>
2023-12-11 09:17:31 -0500

    Remove semicolons

--- src/composable/composable.ts
+++ src/composable/composable.ts
@@ -1,4 +1,4 @@
-import { toErrorWithMessage } from "./errors.ts";
+import { toErrorWithMessage } from './errors.ts'
 import {
   AllArguments,
   CollectArguments,
@@ -15,7 +15,7 @@ import {
   Success,
   UnpackAll,
   UnpackResult,
-} from "./types.ts";
+} from './types.ts'
 
 /**
  * Merges a list of objects into a single object.
@@ -30,15 +30,15 @@ import {
  * //   ^? { a: number, b: number, c: number, d: number }
  */
 function mergeObjects<T extends unknown[] = unknown[]>(objs: T) {
-  return Object.assign({}, ...objs) as MergeObjs<T>;
+  return Object.assign({}, ...objs) as MergeObjs<T>
 }
 
 function success<T>(data: T): Success<T> {
-  return { success: true, data, errors: [] };
+  return { success: true, data, errors: [] }
 }
 
 function error(errors: ErrorWithMessage[]): Failure {
-  return { success: false, errors };
+  return { success: false, errors }
 }
 
 /**
@@ -50,12 +50,12 @@ function composable<T extends Fn>(fn: T): Composable<T> {
   return async (...args) => {
     try {
       // deno-lint-ignore no-explicit-any
-      const result = await fn(...(args as any[]));
-      return success(result);
+      const result = await fn(...(args as any[]))
+      return success(result)
     } catch (e) {
-      return error([toErrorWithMessage(e)]);
+      return error([toErrorWithMessage(e)])
     }
-  };
+  }
 }
 
 /**
@@ -79,9 +79,9 @@ function pipe<T extends [Composable, ...Composable[]]>(
     //@ts-ignore pipe uses exactly he same generic input type as sequence
     //           I don't understand what is the issue here but ignoring the errors
     //           is safe and much nicer than a bunch of casts to any
-    const res = (await sequence(...fns)(...args)) as Result<unknown[]>;
-    return !res.success ? error(res.errors) : success(res.data.at(-1));
-  }) as PipeReturn<T>;
+    const res = (await sequence(...fns)(...args)) as Result<unknown[]>
+    return !res.success ? error(res.errors) : success(res.data.at(-1))
+  }) as PipeReturn<T>
 }
 
 /**
@@ -99,18 +99,18 @@ function all<T extends [Composable, ...Composable[]]>(
   ...fns: T & AllArguments<T>
 ) {
   return (async (...args: any) => {
-    const results = await Promise.all(fns.map((fn) => fn(...args)));
+    const results = await Promise.all(fns.map((fn) => fn(...args)))
 
     if (results.some(({ success }) => success === false)) {
-      return error(results.map(({ errors }) => errors).flat());
+      return error(results.map(({ errors }) => errors).flat())
     }
 
-    return success((results as Success<any>[]).map(({ data }) => data));
+    return success((results as Success<any>[]).map(({ data }) => data))
   }) as Composable<
     (...args: Parameters<AllArguments<T>[0]>) => {
-      [key in keyof T]: UnpackResult<ReturnType<Extract<T[key], Composable>>>;
+      [key in keyof T]: UnpackResult<ReturnType<Extract<T[key], Composable>>>
     }
-  >;
+  >
 }
 
 /**
@@ -127,13 +127,13 @@ function collect<T extends Record<string, Composable>>(
   fns: T & CollectArguments<T>,
 ) {
   const fnsWithKey = Object.entries(fns).map(([key, cf]) =>
-    map(cf, (result) => ({ [key]: result }))
-  );
+    map(cf, (result) => ({ [key]: result })),
+  )
   return map(all(...(fnsWithKey as any)), mergeObjects) as Composable<
     (...args: Parameters<AllArguments<RecordToTuple<T>>[0]>) => {
-      [key in keyof T]: UnpackResult<ReturnType<Extract<T[key], Composable>>>;
+      [key in keyof T]: UnpackResult<ReturnType<Extract<T[key], Composable>>>
     }
-  >;
+  >
 }
 
 /**
@@ -150,21 +150,21 @@ function sequence<T extends [Composable, ...Composable[]]>(
   ...fns: T & PipeArguments<T>
 ) {
   return (async (...args) => {
-    const [head, ...tail] = fns as T;
+    const [head, ...tail] = fns as T
 
-    const res = await head(...args);
-    if (!res.success) return error(res.errors);
+    const res = await head(...args)
+    if (!res.success) return error(res.errors)
 
-    const result = [res.data];
+    const result = [res.data]
     for await (const fn of tail) {
-      const res = await fn(result.at(-1));
-      if (!res.success) return error(res.errors);
-      result.push(res.data);
+      const res = await fn(result.at(-1))
+      if (!res.success) return error(res.errors)
+      result.push(res.data)
     }
-    return success(result);
+    return success(result)
   }) as Composable<
     (...args: Parameters<Extract<First<T>, Composable>>) => UnpackAll<T>
-  >;
+  >
 }
 
 /**
@@ -181,12 +181,12 @@ function map<T extends Composable, R>(
   mapper: (res: UnpackResult<ReturnType<T>>) => R,
 ) {
   return (async (...args) => {
-    const res = await fn(...args);
-    if (!res.success) return error(res.errors);
-    const mapped = await composable(mapper)(res.data);
-    if (!mapped.success) return error(mapped.errors);
-    return mapped;
-  }) as Composable<(...args: Parameters<T>) => R>;
+    const res = await fn(...args)
+    if (!res.success) return error(res.errors)
+    const mapped = await composable(mapper)(res.data)
+    if (!mapped.success) return error(mapped.errors)
+    return mapped
+  }) as Composable<(...args: Parameters<T>) => R>
 }
 
 /**
@@ -201,12 +201,12 @@ function map<T extends Composable, R>(
  */
 function mapError<T extends Composable, R>(
   fn: T,
-  mapper: (err: Omit<Failure, "success">) => Omit<Failure, "success">,
+  mapper: (err: Omit<Failure, 'success'>) => Omit<Failure, 'success'>,
 ) {
   return (async (...args) => {
-    const res = await fn(...args);
-    return !res.success ? error(mapper(res).errors) : success(res.data);
-  }) as T;
+    const res = await fn(...args)
+    return !res.success ? error(mapper(res).errors) : success(res.data)
+  }) as T
 }
 
 export {
@@ -220,4 +220,5 @@ export {
   pipe,
   sequence,
   success,
-};
+}
+
--- src/composable/index.test.ts
+++ src/composable/index.test.ts
@@ -1,164 +1,164 @@
-import { assertEquals, describe, it } from "../test-prelude.ts";
-import { map, mapError, pipe, sequence } from "./index.ts";
-import type { Composable, ErrorWithMessage, Result } from "./index.ts";
-import { Equal, Expect } from "./types.test.ts";
-import { all, collect, composable } from "./composable.ts";
+import { assertEquals, describe, it } from '../test-prelude.ts'
+import { map, mapError, pipe, sequence } from './index.ts'
+import type { Composable, ErrorWithMessage, Result } from './index.ts'
+import { Equal, Expect } from './types.test.ts'
+import { all, collect, composable } from './composable.ts'
 
-const voidFn = composable(() => {});
-const toString = composable((a: unknown) => `${a}`);
-const append = composable((a: string, b: string) => `${a}${b}`);
-const add = composable((a: number, b: number) => a + b);
-const asyncAdd = (a: number, b: number) => Promise.resolve(a + b);
+const voidFn = composable(() => {})
+const toString = composable((a: unknown) => `${a}`)
+const append = composable((a: string, b: string) => `${a}${b}`)
+const add = composable((a: number, b: number) => a + b)
+const asyncAdd = (a: number, b: number) => Promise.resolve(a + b)
 const faultyAdd = composable((a: number, b: number) => {
-  if (a === 1) throw new Error("a is 1");
-  return a + b;
-});
+  if (a === 1) throw new Error('a is 1')
+  return a + b
+})
 const alwaysThrow = composable(() => {
-  throw new Error("always throw", { cause: "it was made for this" });
-});
+  throw new Error('always throw', { cause: 'it was made for this' })
+})
 
-describe("composable", () => {
-  it("infers the types if has no arguments or return", async () => {
-    const fn = composable(() => {});
-    const res = await fn();
+describe('composable', () => {
+  it('infers the types if has no arguments or return', async () => {
+    const fn = composable(() => {})
+    const res = await fn()
 
-    type _FN = Expect<Equal<typeof fn, Composable<() => void>>>;
-    type _R = Expect<Equal<typeof res, Result<void>>>;
+    type _FN = Expect<Equal<typeof fn, Composable<() => void>>>
+    type _R = Expect<Equal<typeof res, Result<void>>>
 
-    assertEquals(res, { success: true, data: undefined, errors: [] });
-  });
+    assertEquals(res, { success: true, data: undefined, errors: [] })
+  })
 
-  it("infers the types if has arguments and a return", async () => {
-    const fn = add;
-    const res = await fn(1, 2);
-
-    type _FN = Expect<
-      Equal<typeof fn, Composable<(a: number, b: number) => number>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<number>>>;
-
-    assertEquals(res, { success: true, data: 3, errors: [] });
-  });
-
-  it("infers the types of async functions", async () => {
-    const fn = composable(asyncAdd);
-    const res = await fn(1, 2);
+  it('infers the types if has arguments and a return', async () => {
+    const fn = add
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => number>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<number>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<number>>>
 
-    assertEquals(res, { success: true, data: 3, errors: [] });
-  });
+    assertEquals(res, { success: true, data: 3, errors: [] })
+  })
 
-  it("catch errors", async () => {
-    const fn = faultyAdd;
-    const res = await fn(1, 2);
+  it('infers the types of async functions', async () => {
+    const fn = composable(asyncAdd)
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => number>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<number>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<number>>>
 
-    assertEquals(res.success, false);
-    assertEquals(res.errors![0].message, "a is 1");
-  });
-});
+    assertEquals(res, { success: true, data: 3, errors: [] })
+  })
 
-describe("pipe", () => {
-  it("sends the results of the first function to the second and infers types", async () => {
-    const fn = pipe(add, toString);
-    const res = await fn(1, 2);
+  it('catch errors', async () => {
+    const fn = faultyAdd
+    const res = await fn(1, 2)
+
+    type _FN = Expect<
+      Equal<typeof fn, Composable<(a: number, b: number) => number>>
+    >
+    type _R = Expect<Equal<typeof res, Result<number>>>
+
+    assertEquals(res.success, false)
+    assertEquals(res.errors![0].message, 'a is 1')
+  })
+})
+
+describe('pipe', () => {
+  it('sends the results of the first function to the second and infers types', async () => {
+    const fn = pipe(add, toString)
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => string>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<string>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<string>>>
 
-    assertEquals(res, { success: true, data: "3", errors: [] });
-  });
+    assertEquals(res, { success: true, data: '3', errors: [] })
+  })
 
-  it("catches the errors from function A", async () => {
-    const fn = pipe(faultyAdd, toString);
-    const res = await fn(1, 2);
+  it('catches the errors from function A', async () => {
+    const fn = pipe(faultyAdd, toString)
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => string>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<string>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<string>>>
 
-    assertEquals(res.success, false);
-    assertEquals(res.errors![0].message, "a is 1");
-  });
+    assertEquals(res.success, false)
+    assertEquals(res.errors![0].message, 'a is 1')
+  })
 
-  it("catches the errors from function B", async () => {
+  it('catches the errors from function B', async () => {
     //@ts-expect-error alwaysThrow won't type-check the composition since its return type is never and toString expects an unknown parameter
-    const fn = pipe(add, alwaysThrow, toString);
+    const fn = pipe(add, alwaysThrow, toString)
     //@ts-expect-error alwaysThrow won't type-check the composition since its return type is never and toString expects an unknown parameter
-    const res = await fn(1, 2);
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       //@ts-expect-error alwaysThrow won't type-check the composition since its return type is never and toString expects an unknown parameter
       Equal<typeof fn, Composable<(a: number, b: number) => string>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<string>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<string>>>
 
-    assertEquals(res.success, false);
-    assertEquals(res.errors![0].message, "always throw");
+    assertEquals(res.success, false)
+    assertEquals(res.errors![0].message, 'always throw')
     assertEquals(
       // deno-lint-ignore no-explicit-any
       (res.errors[0] as any).exception?.cause,
-      "it was made for this",
-    );
-  });
-});
+      'it was made for this',
+    )
+  })
+})
 
-describe("sequence", () => {
-  it("sends the results of the first function to the second and saves every step of the result", async () => {
-    const fn = sequence(add, toString);
-    const res = await fn(1, 2);
-
-    type _FN = Expect<
-      Equal<typeof fn, Composable<(a: number, b: number) => [number, string]>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<[number, string]>>>;
-
-    assertEquals(res, { success: true, data: [3, "3"], errors: [] });
-  });
-
-  it("catches the errors from function A", async () => {
-    const fn = sequence(faultyAdd, toString);
-    const res = await fn(1, 2);
+describe('sequence', () => {
+  it('sends the results of the first function to the second and saves every step of the result', async () => {
+    const fn = sequence(add, toString)
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => [number, string]>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<[number, string]>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<[number, string]>>>
 
-    assertEquals(res.success, false);
-    assertEquals(res.errors![0].message, "a is 1");
-  });
-});
+    assertEquals(res, { success: true, data: [3, '3'], errors: [] })
+  })
 
-describe("all", () => {
-  it("executes all functions using the same input returning a tuple with every result when all are successful", async () => {
-    const fn = all(add, toString, voidFn);
+  it('catches the errors from function A', async () => {
+    const fn = sequence(faultyAdd, toString)
+    const res = await fn(1, 2)
 
-    const res = await fn(1, 2);
+    type _FN = Expect<
+      Equal<typeof fn, Composable<(a: number, b: number) => [number, string]>>
+    >
+    type _R = Expect<Equal<typeof res, Result<[number, string]>>>
 
-    assertEquals(res, { success: true, data: [3, "1", undefined], errors: [] });
-  });
-});
+    assertEquals(res.success, false)
+    assertEquals(res.errors![0].message, 'a is 1')
+  })
+})
 
-describe("collect", () => {
-  it("collects the results of an object of Composables into a result with same format", async () => {
+describe('all', () => {
+  it('executes all functions using the same input returning a tuple with every result when all are successful', async () => {
+    const fn = all(add, toString, voidFn)
+
+    const res = await fn(1, 2)
+
+    assertEquals(res, { success: true, data: [3, '1', undefined], errors: [] })
+  })
+})
+
+describe('collect', () => {
+  it('collects the results of an object of Composables into a result with same format', async () => {
     const fn = collect({
       add: add,
       string: toString,
       void: voidFn,
-    });
-    const res = await fn(1, 2);
+    })
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<
@@ -168,59 +168,59 @@ describe("collect", () => {
             a: number,
             b: number,
           ) => {
-            add: number;
-            string: string;
-            void: void;
+            add: number
+            string: string
+            void: void
           }
         >
       >
-    >;
+    >
     type _R = Expect<
       Equal<typeof res, Result<{ add: number; string: string; void: void }>>
-    >;
+    >
 
     assertEquals(res, {
       success: true,
-      data: { add: 3, string: "1", void: undefined },
+      data: { add: 3, string: '1', void: undefined },
       errors: [],
-    });
-  });
+    })
+  })
 
-  it("uses the same arguments for every function", async () => {
+  it('uses the same arguments for every function', async () => {
     //@ts-expect-error add and append parameters are incompatible
     // The runtime will work since passing 1, 2 will be coerced to '1', '2'
     const fn = collect({
       add: add,
       string: append,
-    });
+    })
     //@ts-expect-error add and append parameters are incompatible
-    const res = await fn(1, 2);
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<
         typeof fn,
         Composable<
           (...args: never) => {
-            add: number;
-            string: string;
+            add: number
+            string: string
           }
         >
       >
-    >;
-    type _R = Expect<Equal<typeof res, Result<any>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<any>>>
     assertEquals(res, {
       success: true,
-      data: { add: 3, string: "12" },
+      data: { add: 3, string: '12' },
       errors: [],
-    });
-  });
+    })
+  })
 
-  it("collects the errors in the error array", async () => {
+  it('collects the errors in the error array', async () => {
     const fn = collect({
       error1: faultyAdd,
       error2: faultyAdd,
-    });
-    const res = await fn(1, 2);
+    })
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<
@@ -230,77 +230,78 @@ describe("collect", () => {
             a: number,
             b: number,
           ) => {
-            error1: number;
-            error2: number;
+            error1: number
+            error2: number
           }
         >
       >
-    >;
+    >
     type _R = Expect<
       Equal<typeof res, Result<{ error1: number; error2: number }>>
-    >;
+    >
 
-    assertEquals(res.success, false);
-    assertEquals(res.errors![0].message, "a is 1");
-    assertEquals(res.errors![1].message, "a is 1");
-  });
-});
+    assertEquals(res.success, false)
+    assertEquals(res.errors![0].message, 'a is 1')
+    assertEquals(res.errors![1].message, 'a is 1')
+  })
+})
 
-describe("map", () => {
-  it("maps over an Composable function successful result", async () => {
-    const fn = map(add, (a) => a + 1 === 4);
-    const res = await fn(1, 2);
-
-    type _FN = Expect<
-      Equal<typeof fn, Composable<(a: number, b: number) => boolean>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<boolean>>>;
-
-    assertEquals(res, { success: true, data: true, errors: [] });
-  });
-
-  it("maps over a composition", async () => {
-    const fn = map(pipe(add, toString), (a) => typeof a === "string");
-    const res = await fn(1, 2);
+describe('map', () => {
+  it('maps over an Composable function successful result', async () => {
+    const fn = map(add, (a) => a + 1 === 4)
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => boolean>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<boolean>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<boolean>>>
 
-    assertEquals(res, { success: true, data: true, errors: [] });
-  });
+    assertEquals(res, { success: true, data: true, errors: [] })
+  })
 
-  it("does not do anything when the function fails", async () => {
-    const fn = map(faultyAdd, (a) => a + 1 === 4);
-    const res = await fn(1, 2);
+  it('maps over a composition', async () => {
+    const fn = map(pipe(add, toString), (a) => typeof a === 'string')
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => boolean>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<boolean>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<boolean>>>
 
-    assertEquals(res.success, false);
-    assertEquals(res.errors![0].message, "a is 1");
-  });
-});
+    assertEquals(res, { success: true, data: true, errors: [] })
+  })
+
+  it('does not do anything when the function fails', async () => {
+    const fn = map(faultyAdd, (a) => a + 1 === 4)
+    const res = await fn(1, 2)
+
+    type _FN = Expect<
+      Equal<typeof fn, Composable<(a: number, b: number) => boolean>>
+    >
+    type _R = Expect<Equal<typeof res, Result<boolean>>>
+
+    assertEquals(res.success, false)
+    assertEquals(res.errors![0].message, 'a is 1')
+  })
+})
 
 const cleanError = (err: ErrorWithMessage) => ({
-  message: err.message + "!!!",
-});
-describe("mapError", () => {
-  it("maps over the error results of an Composable function", async () => {
+  message: err.message + '!!!',
+})
+describe('mapError', () => {
+  it('maps over the error results of an Composable function', async () => {
     const fn = mapError(faultyAdd, ({ errors }) => ({
       errors: errors.map(cleanError),
-    }));
-    const res = await fn(1, 2);
+    }))
+    const res = await fn(1, 2)
 
     type _FN = Expect<
       Equal<typeof fn, Composable<(a: number, b: number) => number>>
-    >;
-    type _R = Expect<Equal<typeof res, Result<number>>>;
+    >
+    type _R = Expect<Equal<typeof res, Result<number>>>
 
-    assertEquals(res.success, false);
-    assertEquals(res.errors![0].message, "a is 1!!!");
-  });
-});
+    assertEquals(res.success, false)
+    assertEquals(res.errors![0].message, 'a is 1!!!')
+  })
+})
+
