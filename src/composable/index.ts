671a4d11877bcf6d56ceebcc9e3a67482e0cbb30
Diogo Biazus <diogo@biazus.ca>
2024-03-01 09:41:30 -0500

    Export catch error on cf namespace

--- src/composable/index.ts
+++ src/composable/index.ts
@@ -1,3 +1,12 @@
 export type { Composable, Result, ErrorWithMessage } from './types.ts'
 export { toErrorWithMessage } from './errors.ts'
-export { composable, pipe, map, mapError, sequence, all, collect } from './composable.ts'
+export {
+  catchError,
+  composable,
+  pipe,
+  map,
+  mapError,
+  sequence,
+  all,
+  collect,
+} from './composable.ts'
