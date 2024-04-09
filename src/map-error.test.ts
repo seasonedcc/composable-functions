2dbabfab3d92dbc228a4516cec52615eb6eba94f
Diogo Biazus <diogo@biazus.ca>
2024-04-09 15:06:28 -0400

    All green

--- src/map-error.test.ts
+++ src/map-error.test.ts
@@ -22,49 +22,38 @@ describe('mapError', () => {
   })
 
   it('returns a domain function function that will apply a function over the error of the first one', async () => {
-    const exception = new Error('Number of errors: 0')
     const a = mdf(z.object({ id: z.number() }))(({ id }) => id + 1)
-    const b = (result: Pick<ErrorResult, 'errors'>) => {
-      const nonInputErrors = result.errors.filter(
-        (e) => !(e instanceof InputError),
-      )
-      const inputErrors = result.errors.filter((e) => e instanceof InputError)
+    const errorMapper = (result: Pick<ErrorResult, 'errors'>) => {
       return {
-        errors: [
-          nonInputErrors,
-          new InputError('Number of input errors: ' + inputErrors.length, ''),
-        ],
+        errors: [new Error('Number of errors: ' + result.errors.length)],
       } as Pick<ErrorData, 'errors'>
     }
 
-    const c = mapError(a, b)
+    const c = mapError(a, errorMapper)
     type _R = Expect<Equal<typeof c, DomainFunction<number>>>
 
     assertEquals(
       await c({ invalidInput: '1' }),
       makeErrorResult({
-        errors: [exception, new InputError('Number of input errors: 1', '')],
+        errors: [new Error('Number of errors: 1')],
       }),
     )
   })
 
   it('returns a domain function function that will apply an async function over the error of the first one', async () => {
     const a = mdf(z.object({ id: z.number() }))(({ id }) => id + 1)
-    const b = (result: Pick<ErrorResult, 'errors'>) =>
+    const errorMapper = (result: Pick<ErrorResult, 'errors'>) =>
       Promise.resolve({
         errors: [new Error('Number of errors: ' + result.errors.length)],
       })
 
-    const c = mapError(a, b)
+    const c = mapError(a, errorMapper)
     type _R = Expect<Equal<typeof c, DomainFunction<number>>>
 
     assertEquals(
       await c({ invalidInput: '1' }),
       makeErrorResult({
-        errors: [
-          new Error('Number of errors: 0'),
-          new InputError('Number of input errors: 1', ''),
-        ],
+        errors: [new Error('Number of errors: 1')],
       }),
     )
   })
