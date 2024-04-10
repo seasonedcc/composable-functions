72cd78e1e8b4d8df9824613078c721d94b2af94a
Diogo Biazus <diogo@biazus.ca>
2024-04-10 19:03:06 -0400

    update build npm script

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,5 +1,5 @@
 // ex. scripts/build_npm.ts
-import { build, emptyDir } from 'https://deno.land/x/dnt@0.38.0/mod.ts'
+import { build, emptyDir } from 'https://deno.land/x/dnt@0.40.0/mod.ts'
 import pkg from '../deno.json' assert { type: 'json' }
 
 await emptyDir('./npm')
@@ -15,19 +15,19 @@ await build({
     undici: true,
   },
   package: {
-    name: 'domain-functions',
+    name: 'composable-functions',
     version: pkg.version,
     description:
       'Decouple your business logic from your controllers. With first-class type inference from end to end.',
     license: 'MIT',
     author: 'Seasoned',
     bugs: {
-      url: 'https://github.com/seasonedcc/domain-functions/issues',
+      url: 'https://github.com/seasonedcc/composable-functions/issues',
     },
-    homepage: 'https://github.com/seasonedcc/domain-functions',
+    homepage: 'https://github.com/seasonedcc/composable-functions',
     repository: {
-      type: "git",
-      url: "https://github.com/seasonedcc/domain-functions.git"
+      type: 'git',
+      url: 'https://github.com/seasonedcc/composable-functions.git',
     },
   },
 })
