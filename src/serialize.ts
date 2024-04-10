d4820e222a5408bab959a51578de73b3b9922411
Guga Guichard <gustavoguichard@gmail.com>
2024-04-09 23:13:10 -0300

    Adjust serialize type

--- src/serialize.ts
+++ src/serialize.ts
@@ -1,8 +1,15 @@
 import { EnvironmentError } from './errors.ts'
 import { InputError } from './errors.ts'
 import type { Result } from './types.ts'
+import { Failure } from './types.ts'
+import { Success } from './types.ts'
 
-function serializeResult(result: Result) {
+function serializeResult<T>(result: Result<T>):
+  | (Success<T> & { inputErrors: []; environmentErrors: [] })
+  | (Failure & {
+      inputErrors: Error[]
+      environmentErrors: Error[]
+    }) {
   if (result.success) {
     return {
       ...result,
