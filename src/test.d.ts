368d0299c4ff8ada7c34efb2fb6804305ee2ec50
Guga Guichard <gustavoguichard@gmail.com>
2025-04-05 07:54:43 -0300

    Add a Unequal test to assure we don't have a Promise in the resulting type of an async composable

--- src/test.d.ts
+++ src/test.d.ts
@@ -1,5 +1,8 @@
 type Expect<T extends true> = T
-type Equal<A, B> =
-  // prettier-ignore
-  (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? true
-    : [A, 'should equal', B]
+
+type Equal<A, B> = (<T>() => T extends A ? 1 : 2) extends
+  (<T>() => T extends B ? 1 : 2) ? true
+  : [A, 'should equal', B]
+
+type Unequal<A, B> = Equal<A, B> extends true ? [A, 'should not equal', B]
+  : true
